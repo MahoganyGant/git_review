e81beb45b295254aeebd7f50a105d0f9e1faa3b4
MahoganyGant <mahoganygant02@gmail.com>
2024-09-17 14:47:18 -0400

    commiting changes from feature branch

--- src/main/java/class_assignment/gitignoreProject.java
+++ src/main/java/class_assignment/gitignoreProject.java
@@ -4,5 +4,6 @@ public class gitignoreProject {
 
     public static void main(String[] args) {
         System.out.println("Hello Main branch");
+        System.out.println("Hello from feature branch");
     }
 }
